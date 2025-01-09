63fc8ceccb1b41db868b4e4c383eee2e473137a1
BhaskarMantralaHub <97362616+BhaskarMantralaHub@users.noreply.github.com>
2025-01-08 22:32:55 -0800

    test

--- operators.py
+++ operators.py
@@ -11,5 +11,5 @@ print (1 + 2**3 / 4 * 5 / (10 * 2))
 # 2. Power -> 2**3 -> 8
 # 2. Division -> 8/4 -> 2 (left to right)
 # 3. Multiplication -> (1 + 8/4 * 5 / 20) -> (1 + 10 / 20)
-# 4. Divison -> (1 + 0.5)
+# 4. Division -> (1 + 0.5)
 # 5. Addition -> 1.5
